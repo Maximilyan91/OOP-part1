aba16a0d2004350b9ccb662b27fbcb37ad218d57
Максим <gmax7911@gmail.com>
2023-01-09 14:54:03 +0300

    Задача 2 выполнена

--- src/Automobile.java
+++ src/Automobile.java
@@ -16,4 +16,12 @@ public class Automobile {
 
     }
 
+    public Automobile(String brand, String model, double engineVolume, String color, int productionYear, String productionCountry) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.productionYear = productionYear;
+        this.productionCountry = productionCountry;
+    }
 }
